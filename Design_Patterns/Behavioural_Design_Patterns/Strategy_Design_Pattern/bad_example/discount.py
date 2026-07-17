70a84340003376bf41d277ddcf2659ab21ec788f
Vansh <vanshchoudhary2744@gmail.com>
2026-07-18 00:23:36 +0530

    bad example of strategy pattern

---
+++ Design_Patterns/Behavioural_Design_Patterns/Strategy_Design_Pattern/bad_example/discount.py
@@ -0,0 +1,7 @@
+class Discount:
+    def process_discount(self, discount):
+        if discount == "diwali":
+            print("Processing Diwali Discount")
+        
+        elif discount == "holi":
+            print("Processing Holi Discount")
---
+++ Design_Patterns/Behavioural_Design_Patterns/Strategy_Design_Pattern/bad_example/main.py
@@ -0,0 +1,5 @@
+from discount import Discount
+
+dis = Discount()
+dis.process_discount("diwali")
+dis.process_discount("holi")
